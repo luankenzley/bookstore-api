e18a62580cb1fe700514da5fb6b6f0a36a47022d
luankenzley <luankenzley@hotmail.com>
2021-03-18 18:24:45 -0300

    Exception personalizada para objeto n encontrado

---
+++ src/main/java/com/luankenzley/bookstore/resources/exceptions/StandardError.java
@@ -0,0 +1,45 @@
+package com.luankenzley.bookstore.resources.exceptions;
+
+public class StandardError {
+
+	private Long timestemp;
+	private Integer status;
+	private String error;
+
+	public StandardError() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StandardError(Long timestemp, Integer status, String error) {
+		super();
+		this.timestemp = timestemp;
+		this.status = status;
+		this.error = error;
+	}
+
+	public Long getTimestemp() {
+		return timestemp;
+	}
+
+	public void setTimestemp(Long timestemp) {
+		this.timestemp = timestemp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+}
--- src/main/java/com/luankenzley/bookstore/services/CategoriaService.java
+++ src/main/java/com/luankenzley/bookstore/services/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.luankenzley.bookstore.domain.Categoria;
 import com.luankenzley.bookstore.repositories.CategoriaRepository;
+import com.luankenzley.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
