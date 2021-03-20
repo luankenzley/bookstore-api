79eb53e3d1b010509e759ecf717a65c394a0aa8b
luankenzley <luankenzley@hotmail.com>
2021-03-20 09:15:12 -0300

    findAll para Categoria

--- src/main/java/com/luankenzley/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/luankenzley/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.luankenzley.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luankenzley.bookstore.domain.Categoria;
+import com.luankenzley.bookstore.dtos.CategoriaDTO;
 import com.luankenzley.bookstore.services.CategoriaService;
 
 @RestController
@@ -22,6 +26,14 @@ public class CategoriaResource {
 		Categoria obj = categoriaService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = categoriaService.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 }
 
 // localhost:8080/categorias/{ID}
