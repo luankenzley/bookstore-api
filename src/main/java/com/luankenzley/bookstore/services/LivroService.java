9079a3940bddaa36b2a474beb3596d3ee7966a86
luankenzley <luankenzley@hotmail.com>
2021-03-21 10:49:21 -0300

    implementando DTO E JPQL De livro com categoria

--- src/main/java/com/luankenzley/bookstore/services/LivroService.java
+++ src/main/java/com/luankenzley/bookstore/services/LivroService.java
@@ -1,5 +1,6 @@
 package com.luankenzley.bookstore.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,21 @@ import com.luankenzley.bookstore.services.exceptions.ObjectNotFoundException;
 public class LivroService {
 
 	@Autowired
-	LivroRepository livroRepository;
+	private LivroRepository livroRepository;
+	
+	@Autowired
+    private	CategoriaService categoriaService;
 
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = livroRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! ID:" + id + ", Tipo: " + Livro.class.getName()));
 
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		categoriaService.findById(id_cat);
+		return livroRepository.findAllByCategoria(id_cat);
 	}
 
 }
